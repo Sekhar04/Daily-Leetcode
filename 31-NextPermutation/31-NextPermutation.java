4992e6a2885d8b683287f7b981ba9fb4c2b7f847
Shobhit Sekhar <144170440+Sekhar04@users.noreply.github.com>
2026-01-20 17:28:44 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.java
@@ -0,0 +1,26 @@
+// Last updated: 1/20/2026, 5:28:41 PM
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        int left = i + 1, right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
