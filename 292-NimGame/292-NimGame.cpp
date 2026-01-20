8ca23d2cb4203d69e70374b69d2df1b3f2d29546
Shobhit Sekhar <144170440+Sekhar04@users.noreply.github.com>
2026-01-20 17:27:31 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 1/20/2026, 5:27:28 PM
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        }else 
+        return true;
+    }
+};
