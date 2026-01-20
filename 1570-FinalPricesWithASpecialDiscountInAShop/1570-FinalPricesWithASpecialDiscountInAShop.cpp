1981fad0fa777645743a992b5556c7e3dd523d3f
Shobhit Sekhar <144170440+Sekhar04@users.noreply.github.com>
2026-01-20 17:26:40 +0530

    Create 1570-FinalPricesWithASpecialDiscountInAShop

---
+++ 1570-FinalPricesWithASpecialDiscountInAShop/1570-FinalPricesWithASpecialDiscountInAShop.cpp
@@ -0,0 +1,18 @@
+// Last updated: 1/20/2026, 5:26:38 PM
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> ans = prices;
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && prices[st.top()] >= prices[i]) {
+                ans[st.top()] -= prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
